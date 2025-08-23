f9713d41667c38291918db7d0641735de709b75f
megabytis <166828632+megabytis@users.noreply.github.com>
2025-08-23 21:26:16 +0530

    created a POST API for signup

--- BACKEND/src/app.js
+++ BACKEND/src/app.js
@@ -1,14 +1,32 @@
 const express = require("express");
 const { connectDB, mongoose } = require("./config/database");
+const { UserModel } = require("./models/user");
 
 const app = express();
+app.use(express.json());
+
+// creating API
+app.post("/signup", async (req, res) => {
+  try {
+    // const User = new UserModel({
+    //   firstName: "Lalu",
+    //   lastName: "Yadav",
+    //   age: 51,
+    //   email: "lallu@yadav.com",
+    // });
+
+    await User.save();
+    res.send("User added successfully");
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 connectDB()
   .then(() => {
     console.log("DB connected to app");
-    app.listen(3000, () => {
-      console.log("App is listening on port 3000");
+    app.listen(8888, () => {
+      console.log("App is listening on port 8888");
     });
   })
-  .catch((err) => console.log(err))
-  .finally(mongoose.disconnect());
+  .catch((err) => console.log(err));
