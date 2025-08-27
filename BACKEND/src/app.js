0e71e7ecc4afcdd957c16d01f583746449041778
megabytis <166828632+megabytis@users.noreply.github.com>
2025-08-27 22:18:47 +0530

    added APIs for CRUD operations

--- BACKEND/src/app.js
+++ BACKEND/src/app.js
@@ -5,20 +5,66 @@ const { UserModel } = require("./models/user");
 const app = express();
 app.use(express.json());
 
-// creating API
-app.post("/signup", async (req, res) => {
-  // const User = new UserModel({
-  //   firstName: "Lalu",
-  //   lastName: "Yadav",
-  //   age: 51,
-  //   email: "lallu@yadav.com",
-  // });
+// CREATE
+// creating SIGNUP API
+app.post("/sign-up", async (req, res) => {
+  const { firstName, lastName, age, email } = req.body;
+
+  const user = new UserModel({
+    firstName: firstName,
+    lastName: lastName,
+    age: age,
+    email: email,
+  });
+
+  console.log(req.body);
 
   try {
-    await User.save();
+    await user.save();
     res.send("User added successfully");
   } catch (err) {
     console.log(err);
+  }
+});
+
+// READ
+// creating FEED API :- to get all users data from database
+app.get("/feed", async (req, res, next) => {
+  try {
+    const foundUser = await UserModel.findOne({ email: req.body.email });
+    if (foundUser.length === 0) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(foundUser);
+    }
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+// UPDATE
+// updating a user data
+app.patch("/user", async (req, res) => {
+  const updatedUser = req.body;
+  const userID = req.body.userID;
+
+  try {
+    await UserModel.findByIdAndUpdate(userID, updatedUser);
+    res.send("User updated Successfully");
+  } catch (err) {
+    res.status(400).send("Error ocurred");
+  }
+});
+
+// DELETE
+// deleting a user from DB
+app.delete("/user", async (req, res) => {
+  try {
+    // await UserModel.findByIdAndDelete({ _id: req.body.userID }); // OR
+    await UserModel.findByIdAndDelete(req.body.userID);
+    res.send("user Deleted successfully");
+  } catch (err) {
+    res.status(400).send("Error Occurred");
   }
 });
 
