b6590b4975dbbb0d2080f959e36121f218d77e29
megabytis <166828632+megabytis@users.noreply.github.com>
2025-08-29 22:41:05 +0530

    update in app
    added Schema Types like required, minLength, maxLength, lowercase, uppercase, trim, default etc...

--- BACKEND/src/models/user.js
+++ BACKEND/src/models/user.js
@@ -1,10 +1,39 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  firstName: String,
-  lastName: String,
-  email: String,
-  age: Number,
+  firstName: {
+    type: String,
+    required: true,
+    minLength: 5,
+    maxLength: 30,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    lowercase: true,
+    trim: true,
+  },
+  age: {
+    type: Number,
+    min: 18,
+  },
+  photoURL: {
+    type: String,
+    default:
+      "https://cdn1.iconfinder.com/data/icons/user-pictures/100/unknown-512.png",
+  },
+  skills: {
+    type: [String],
+    default: ["driving", "cooking"],
+  },
+  about: {
+    type: String,
+    default: "Hey There! I'm using devTinder",
+  },
 });
 
 const UserModel = mongoose.model("User", userSchema);
