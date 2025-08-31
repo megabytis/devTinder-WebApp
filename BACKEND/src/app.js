cd65a8cb5529b7acf5c80940857a065dc16dea98
megabytis <166828632+megabytis@users.noreply.github.com>
2025-08-31 12:34:20 +0530

    created /login API

--- BACKEND/src/app.js
+++ BACKEND/src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const validator = require("validator");
+const bcrypt = require("bcrypt");
 const { connectDB, mongoose } = require("./config/database");
 const { UserModel } = require("./models/user");
 const { validateSignupData } = require("./utils/validate");
@@ -10,9 +11,6 @@ app.use(express.json());
 // CREATE
 // creating SIGNUP API
 app.post("/sign-up", async (req, res, next) => {
-  // 1st Validating Data
-  validateSignupData(req);
-
   const {
     firstName,
     lastName,
@@ -22,17 +20,25 @@ app.post("/sign-up", async (req, res, next) => {
     skills,
     photoURL,
     gender,
+    about,
   } = req.body;
+
+  // 1st Validating Data
+  validateSignupData(req);
+
+  // 2nd Encrypting the Password
+  const hashedPassword = await bcrypt.hash(password, 10);
 
   const user = new UserModel({
     firstName: firstName,
     lastName: lastName,
     age: age,
     email: email,
-    password: password,
+    password: hashedPassword,
     skills: skills,
     photoURL: photoURL,
     gender: gender,
+    about: about,
   });
 
   try {
@@ -43,18 +49,44 @@ app.post("/sign-up", async (req, res, next) => {
   }
 });
 
+// creating LOGIN API
+app.post("/login", async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    // first checking wheather the email id is present in the DataBase or not
+    const foundUserData = await UserModel.findOne({ email: email });
+    if (!foundUserData) {
+      throw new Error("Invalid Creadential!");
+    }
+
+    const isPasswordSame = await bcrypt.compare(
+      password,
+      foundUserData.password
+    );
+
+    if (isPasswordSame) {
+      res.send("Login Successful!");
+    } else {
+      throw new Error("Invalid Creadential!");
+    }
+  } catch (err) {
+    next(err);
+  }
+});
+
 // READ
 // creating FEED API :- to get all users data from database
-app.get("/feed", async (req, res) => {
+app.get("/feed", async (req, res, next) => {
   try {
     const foundUser = await UserModel.findOne({ email: req.body.email });
     if (foundUser.length === 0) {
-      res.status(404).send("User not found");
+      res.status(404).send("Invalid Creadential!");
     } else {
       res.send(foundUser);
     }
   } catch (err) {
-    res.status(400).send("Something went wrong");
+    next(err);
   }
 });
 
@@ -71,6 +103,7 @@ app.patch("/user/:userID", async (req, res) => {
       "age",
       "photoURL",
       "skills",
+      "about",
     ];
 
     const isUpdateAllowed = Object.keys(data).every((k) =>
@@ -92,18 +125,18 @@ app.patch("/user/:userID", async (req, res) => {
 
 // DELETE
 // deleting a user from DB
-app.delete("/user", async (req, res) => {
+app.delete("/user", async (req, res, next) => {
   try {
     // await UserModel.findByIdAndDelete({ _id: req.body.userID }); // OR
     await UserModel.findByIdAndDelete(req.body.userID);
     res.send("user Deleted successfully");
   } catch (err) {
-    res.status(400).send("Error ocurred");
+    next(err);
   }
 });
 
 // Global Error Handler middleWare
-app.use((err, re, res, next) => {
+app.use((err, req, res, next) => {
   // console.log(err); // for debugging purpose
   return res.status(err.statusCode || 500).send(`ERROR: ${err.message}`);
 });
