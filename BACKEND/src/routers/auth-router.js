a585dd950582651f3a241bb5373747d1e85cc687
megabytis <166828632+megabytis@users.noreply.github.com>
2025-09-05 11:38:08 +0530

    removing /login /sign-up APIs from app.js and writing them under express-router

---
+++ BACKEND/src/routers/auth-router.js
@@ -0,0 +1,81 @@
+const express = require("express");
+
+const authRouter = express.Router();
+
+authRouter.post("/sign-up", async (req, res, next) => {
+  const {
+    firstName,
+    lastName,
+    age,
+    email,
+    password,
+    skills,
+    photoURL,
+    gender,
+    about,
+  } = req.body;
+
+  // 1st Validating Data
+  validateSignupData(req);
+
+  // 2nd Encrypting the Password
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const user = new UserModel({
+    firstName: firstName,
+    lastName: lastName,
+    age: age,
+    email: email,
+    password: hashedPassword,
+    skills: skills,
+    photoURL: photoURL,
+    gender: gender,
+    about: about,
+  });
+
+  try {
+    await user.save();
+    res.send("User added successfully");
+  } catch (err) {
+    next(err);
+  }
+});
+
+authRouter.post("/login", async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    // 1️⃣ Find User
+    // first checking wheather the email id is present in the DataBase or not
+    const foundUserData = await UserModel.findOne({ email: email });
+    if (!foundUserData) {
+      throw new Error("Invalid Credential!");
+    }
+
+    // 2️⃣ Validate password
+    const isPasswordSame = await foundUserData.validatePassword(password);
+
+    if (isPasswordSame) {
+      // if password is valid then ;
+      // 3️⃣ Generate JWT via schema method
+      const token = await foundUserData.getJWT();
+
+      // Add the token to Cookie & then send the response back
+      res.cookie("token", token, {
+        httpOnly: true,
+        maxAge: 60 * 60 * 1000, // 1 hour
+        sameSite: "strict", // CSRF protection
+      });
+
+      res.send("Login Successful!");
+    } else {
+      throw new Error("Invalid Credential!");
+    }
+  } catch (err) {
+    next(err);
+  }
+});
+
+module.exports = {
+  router,
+};
