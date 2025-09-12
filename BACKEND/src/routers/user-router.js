de035ae5c8bfd57f4b980327cf2252926b18fa44
Madhusudan Bhukta <166828632+megabytis@users.noreply.github.com>
2025-09-12 11:31:24 +0530

    update

--- BACKEND/src/routers/user-router.js
+++ BACKEND/src/routers/user-router.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { userAuth } = require("../middleware/Auth");
 const { connectionRequestModel } = require("../models/connectionRequest");
+const { UserModel } = require("../models/user");
 
 const userRouter = express.Router();
 
@@ -24,7 +25,17 @@ userRouter.get("/user/requests/recieved", userAuth, async (req, res, next) => {
       status: "interested",
       toUserID: user._id,
     });
-    res.send(foundDocs);
+    res.json({ message: `All requests Recieved.`, data: foundDocs });
+  } catch (err) {
+    next(err);
+  }
+});
+
+userRouter.get("/user/feed", userAuth, async (req, res, next) => {
+  try {
+    const user = req.user;
+    const docs = await UserModel.find({ _id: { $ne: user._id } });
+    res.json({ message: `Feeds`, data: docs });
   } catch (err) {
     next(err);
   }
