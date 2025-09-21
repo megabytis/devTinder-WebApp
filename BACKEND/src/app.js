2346c89027e3be3bc464bc6a53b5e99e8b022798
Madhusudan Bhukta <166828632+megabytis@users.noreply.github.com>
2025-09-21 20:32:06 +0530

    .

--- BACKEND/src/app.js
+++ BACKEND/src/app.js
@@ -38,19 +38,19 @@ app.use(
 
 // ✅ Handle preflight requests for all routes
 // Handle preflight requests for all routes
-app.options("/*", (req, res) => {
-  res.header("Access-Control-Allow-Origin", req.headers.origin);
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header(
-    "Access-Control-Allow-Methods",
-    "GET, PUT, POST, DELETE, PATCH, OPTIONS"
-  );
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization, X-Requested-With"
-  );
-  res.sendStatus(200);
-});
+// app.options("/*", (req, res) => {
+//   res.header("Access-Control-Allow-Origin", req.headers.origin);
+//   res.header("Access-Control-Allow-Credentials", true);
+//   res.header(
+//     "Access-Control-Allow-Methods",
+//     "GET, PUT, POST, DELETE, PATCH, OPTIONS"
+//   );
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Content-Type, Authorization, X-Requested-With"
+//   );
+//   res.sendStatus(200);
+// });
 
 // ✅ Force credentials header
 app.use((req, res, next) => {
