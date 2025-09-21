d233c1ca75a5bae7ad4ce8e1d338b55cc55fd196
Madhusudan Bhukta <166828632+megabytis@users.noreply.github.com>
2025-09-21 12:12:23 +0530

    .

--- BACKEND/src/config/database.js
+++ BACKEND/src/config/database.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   await mongoose.connect(
-    "mongodb://127.0.0.1:27017/devTinder"
-    // "mongodb+srv://devTinder-DB:fzXGIEG5JCkItaOZ@devtinder.yzcimn0.mongodb.net/devTinder-DB"
+    // "mongodb://127.0.0.1:27017/devTinder"
+    "mongodb+srv://devTinder-DB:zOIiecBlsS98Ir2o@devtinder.yzcimn0.mongodb.net/devTinder-DB"
   );
 };
 
