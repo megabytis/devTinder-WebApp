337c8d34797ba99997be862a4738518fa909e6cb
Madhusudan Bhukta <166828632+megabytis@users.noreply.github.com>
2025-09-22 10:53:14 +0530

    .

--- BACKEND/src/app.js
+++ BACKEND/src/app.js
@@ -20,7 +20,7 @@ const app = express();
 // ✅ CORS config - UPDATED
 app.use(
   cors({
-    origin: ["https://dev-tinder-web-app-ivory.vercel.app/"],
+    origin: ["https://dev-tinder-web-app-six.vercel.app/"],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     credentials: true,
     // allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
